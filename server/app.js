3588c167d0836176209d82d3702a386de094dd08
Shaneg13 <sgrewell713@gmail.com>
2022-12-16 10:36:39 -0500

    Adding updates for week 8 homework

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const { request } = require("http");
+const { response } = require("express");
+
+dotenv.config();
+
+// Initialize the Express application
+const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
+app.get("/status", (request, response) => {
+  response.status(200).json({ message: "Service Healthy" });
+});
+
+app.get("/users/:id", (request, response) => {
+  // express adds a "params" Object to requests
+  const id = request.params.id;
+
+  // handle GET request for post with an id of "id"
+  response.send(JSON.stringify({ user_id: is }));
+});
+
+app.post("/add", (request, response) => {
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2,
+  };
+  response.json(responseBody);
+});
+
+const PORT = process.env.PORT || 4040;
+
+// Tell the Express app to start listening
+// Let the humans know I am running and listening on 4040
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- store/index.js
+++ store/index.js
@@ -4,3 +4,4 @@ export { default as Links } from "./Links";
 export { default as Direction } from "./Direction";
 export { default as Route } from "./Route";
 export { default as Map } from "./Map";
+//sexport { default as Google } from "./Google";
